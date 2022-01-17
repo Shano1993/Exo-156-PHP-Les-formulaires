c229671f666707769d3d446897534b94962b83d8
Shano1993 <stefan.hanotiau@gmail.com>
2022-01-17 16:04:23 +0100

    form

---
+++ form.php
@@ -0,0 +1,10 @@
+<?php
+
+if ($_POST['username'] === "James") {
+    echo "Bond, James Bond";
+}
+else {
+    echo "Bonjour " . $_POST['username'];
+}
+
+
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Exo 156</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <form method="post" action="form.php">
+        <div>
+            <label for="id-username">Votre prénom</label>
+            <input type="text" name="username" id="id-username">
+        </div>
+
+        <div>
+            <input type="submit" value="Envoyer">
+        </div>
+    </form>
+</body>
+</html>
